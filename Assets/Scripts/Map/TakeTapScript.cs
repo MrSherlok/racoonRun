18115fd54743f16d1f9cbfe87b810eded1f6b316
MeMike <antomike@mail.ru>
2016-03-14 02:31:02 +0200

    add drag

--- Assets/Scripts/Map/TakeTapScript.cs
+++ Assets/Scripts/Map/TakeTapScript.cs
@@ -1,7 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class TakeTapScript : MonoBehaviour {
+
+public class TakeTapScript : MonoBehaviour, IDragHandler, IBeginDragHandler
+{
+     Transform map;
+
+    void Start()
+    {
+        map = transform.GetChild(0);    // получаем ссылку на Transform зеленой панели.
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+        public void OnDrag(PointerEventData eventData)
+        {
+        if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
+        {
+            if (eventData.delta.x > 0) GameObject.Find("Text").GetComponent<Text>().text = "Right";
+            else GameObject.Find("Text").GetComponent<Text>().text = "Left";
+
+            map.position += new Vector3(eventData.delta.x, 0, 0);
+        }
+        else
+        {
+            if (eventData.delta.y > 0) GameObject.Find("Text").GetComponent<Text>().text = "Up";
+            else GameObject.Find("Text").GetComponent<Text>().text = "Down";
+
+            map.position += new Vector3(0, eventData.delta.y, 0);
+        }
+
+    }
+}
 
 /*	public bool isCentury19Active = false;
 
@@ -9,4 +42,4 @@ public class TakeTapScript : MonoBehaviour {
 		isCentury19Active = true;
 	}
 */
-}
+

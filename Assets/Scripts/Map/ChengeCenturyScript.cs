bf530886e030b2efdaae04a2cb351c1f7d0f7833
MeMike <antomike@mail.ru>
2016-03-18 00:39:52 +0200

    change choose lvl system  and add swipe

--- Assets/Scripts/Map/ChengeCenturyScript.cs
+++ Assets/Scripts/Map/ChengeCenturyScript.cs
@@ -3,55 +3,55 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ChengeCenturyScript : MonoBehaviour {
-	public Camera mainCam;
-	public Camera century19Cam;
-	public Image century19Image;
-	public Image century19Lvl1Image;
-//	private TakeTapScript takeTap;
-//	private bool isCentury19ActiveChenge = false;
-	public Text century19Txt;
-	public Text century19Lvl1Txt;
-	public Image backMMImage;
-	public Image backToCenturyImage;
+//	public Camera mainCam;
+//	public Camera century19Cam;
+//	public Image century19Image;
+//	public Image century19Lvl1Image;
+////	private TakeTapScript takeTap;
+////	private bool isCentury19ActiveChenge = false;
+//	public Text century19Txt;
+//	public Text century19Lvl1Txt;
+//	public Image backMMImage;
+//	public Image backToCenturyImage;
 	
-	public bool isCentury19Active = false;
+//	public bool isCentury19Active = false;
 
-	void FixedUpdate() {
+//	void FixedUpdate() {
 
 
-		//TakeTapScript takeTap = GetComponent<TakeTapScript> ();
-	//	isCentury19ActiveChenge = GetComponent<TakeTapScript> ().isCentury19Active;
+//		//TakeTapScript takeTap = GetComponent<TakeTapScript> ();
+//	//	isCentury19ActiveChenge = GetComponent<TakeTapScript> ().isCentury19Active;
 
-		if (isCentury19Active == true) {
-			backMMImage.enabled = false;
-			backToCenturyImage.enabled = true;
-			century19Txt.enabled = false;
-			century19Lvl1Txt.enabled = true;
-			century19Lvl1Image.enabled = true;
-			century19Image.enabled = false;
-			century19Cam.enabled = true;
-			mainCam.enabled = false;
-		} else {
-			backMMImage.enabled = true;
-			backToCenturyImage.enabled = false;
-			century19Txt.enabled = true;
-			century19Lvl1Txt.enabled = false;
-			century19Lvl1Image.enabled = false;
-			century19Image.enabled = true;
-			century19Cam.enabled = false;
-			mainCam.enabled = true;
-		}
-	}
+//		if (isCentury19Active == true) {
+//			backMMImage.enabled = false;
+//			backToCenturyImage.enabled = true;
+//			century19Txt.enabled = false;
+//			century19Lvl1Txt.enabled = true;
+//			century19Lvl1Image.enabled = true;
+//			century19Image.enabled = false;
+//			century19Cam.enabled = true;
+//			mainCam.enabled = false;
+//		} else {
+//			backMMImage.enabled = true;
+//			backToCenturyImage.enabled = false;
+//			century19Txt.enabled = true;
+//			century19Lvl1Txt.enabled = false;
+//			century19Lvl1Image.enabled = false;
+//			century19Image.enabled = true;
+//			century19Cam.enabled = false;
+//			mainCam.enabled = true;
+//		}
+//	}
 
-	public void Century19Active () {
-		isCentury19Active = true;
-	}
+//	public void Century19Active () {
+//		isCentury19Active = true;
+//	}
 
 	public void Century19Lvl1 () {
 		Application.LoadLevel (2);
 	}
 
-	public void BackToChooseCentury () {
-		isCentury19Active = false;
-	}
+	//public void BackToChooseCentury () {
+	//	isCentury19Active = false;
+	//}
 }
--- Assets/Scripts/Map/TakeTapScript.cs
+++ Assets/Scripts/Map/TakeTapScript.cs
@@ -18,20 +18,22 @@ public class TakeTapScript : MonoBehaviour, IDragHandler, IBeginDragHandler
     }
         public void OnDrag(PointerEventData eventData)
         {
-        if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
-        {
-            if (eventData.delta.x > 0) GameObject.Find("Text").GetComponent<Text>().text = "Right";
-            else GameObject.Find("Text").GetComponent<Text>().text = "Left";
+        //if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
+        //{
+        //    if (eventData.delta.x > 0) GameObject.Find("Text").GetComponent<Text>().text = "Right";
+        //    else GameObject.Find("Text").GetComponent<Text>().text = "Left";
 
-            map.position += new Vector3(eventData.delta.x, 0, 0);
-        }
-        else
-        {
-            if (eventData.delta.y > 0) GameObject.Find("Text").GetComponent<Text>().text = "Up";
-            else GameObject.Find("Text").GetComponent<Text>().text = "Down";
+        //    map.position += new Vector3(eventData.delta.x, 0, 0);
+        //}
+        //else
+        //{
+        //    if (eventData.delta.y > 0) GameObject.Find("Text").GetComponent<Text>().text = "Up";
+        //    else GameObject.Find("Text").GetComponent<Text>().text = "Down";
 
-            map.position += new Vector3(0, eventData.delta.y, 0);
-        }
+        //    map.position += new Vector3(0, eventData.delta.y, 0);
+        //}
+
+        map.position += new Vector3(eventData.delta.x, eventData.delta.y, 0);
 
     }
 }

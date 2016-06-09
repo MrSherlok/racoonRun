6a9b5d5d8cb8f56401b36bd595e11bab9d035055
Germetic <nazarvkvk@Mail.ru>
2016-06-09 17:00:39 +0300

    New Camera System

---
+++ Assets/Scripts/CameraScripts/CameraFollow3.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow3 : MonoBehaviour {
+
+	public GameObject camTargetObject;
+	[SerializeField]
+	float ppc;
+	void Start(){
+		//camTargetObject = GameObject.Find("Player");
+
+			
+	}
+	void Update () {
+		ppc = camTargetObject.transform.localPosition.y - 19.69f;
+		//if (camTargetObject.transform.position.y)
+
+		gameObject.transform.position = new Vector3(camTargetObject.transform.position.x,ppc*-0.35f,-10f);
+
+
+	}
+}
--- Assets/Scripts/newSystem/BoxingGlowScript.cs
+++ Assets/Scripts/newSystem/BoxingGlowScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BoxingGlowScript : MonoBehaviour {
 		
 	void FixedUpdate () {
-		gameObject.transform.position = GameObject.Find ("GlowPoint").transform.position;
-		gameObject.transform.rotation = GameObject.Find ("GlowPoint").transform.rotation;
+//		gameObject.transform.position = GameObject.Find ("GlowPoint").transform.position;
+		//gameObject.transform.rotation = GameObject.Find ("GlowPoint").transform.rotation;
 	}
 }

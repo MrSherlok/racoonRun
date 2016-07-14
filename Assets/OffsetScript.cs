c4dc5deac3c6520a6dfca0a0e7ccd3b503422817
Germetic <nazarvkvk@Mail.ru>
2016-07-14 16:03:13 +0300

    -TutorialLocation

---
+++ Assets/OffsetScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScript : MonoBehaviour {
+
+	public bool x = true;
+	public bool y = false;
+
+	void Update () {
+		gameObject.GetComponent<Renderer> ().material.mainTextureOffset = new Vector2(Time.frameCount,0);
+	}
+}

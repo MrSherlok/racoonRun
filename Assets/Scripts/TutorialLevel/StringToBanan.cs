f5a76bfce683a9c3dfeba6ea5cb9467ce97250d8
MrSherlok <antomike@mail.ru>
2016-07-26 17:54:52 +0300

    standart lvl

--- Assets/Scripts/TutorialLevel/StringToBanan.cs
+++ Assets/Scripts/TutorialLevel/StringToBanan.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 public class StringToBanan : MonoBehaviour {
 	public GameObject arrowToBananGun;
 	void Start(){
-		arrowToBananGun.SetActive (false);
+		//arrowToBananGun.SetActive (false);
 	}
 	void OnTriggerEnter2D(Collider2D col){
 		if (col.gameObject.tag == "Player") {
-			arrowToBananGun.SetActive (true);
-			GameObject.Find ("bananaGun").GetComponent<Image>().enabled = true;
+			arrowToBananGun.GetComponent<Image>().enabled = true;
+//			GameObject.Find ("bananaGun").GetComponent<Image>().enabled = true;
 			GameObject.Find ("shootpoint").GetComponent<BananaGun> ().enabled = true;
 		}
 	}
 	void OnTriggerExit2D(Collider2D col){
 		if (col.gameObject.tag == "Player") {
-			arrowToBananGun.SetActive(false);
+			//arrowToBananGun.SetActive(false);
 		}
 	}
 

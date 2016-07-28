0c8139ffaedfa7f6f9d89d62a54a7b480902eef2
Germetic <nazarvkvk@Mail.ru>
2016-07-28 17:18:11 +0300

    New targer raycast system

---
+++ Assets/DelReycasttesting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelReycasttesting : MonoBehaviour {
+	public Transform target;
+	public Transform hitPoint;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast (gameObject.transform.position,transform.up);
+		hitPoint.transform.position = hit.point;
+		Debug.DrawLine(gameObject.transform.position,hit.point);
+	}
+}
---
+++ Assets/RaycastDraw.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastDraw : MonoBehaviour {
+	private LineRenderer lineRenderer;
+	private Transform target;
+	void Start(){
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.useWorldSpace = true;
+		lineRenderer.enabled = false;
+		lineRenderer.sortingLayerName = "Enemy";
+		target = GameObject.Find ("enemyFinder").transform;
+	}
+	void Update () {
+		//RaycastHit2D hit = Physics2D.Raycast(transform.position, target.transform.position);
+		//Debug.DrawLine (transform.position, target.transform.position);
+		//laserHit.position = hit.point;
+
+		lineRenderer.SetPosition(0,transform.position);
+		lineRenderer.SetPosition(1,target.transform.position);
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag == "enemyFinder") {
+			lineRenderer.enabled = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D col){
+			if (col.tag == "enemyFinder") {
+				lineRenderer.enabled = false;
+			}
+	}
+}

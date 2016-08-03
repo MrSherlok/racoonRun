41eb1e80604fb6d242262787c64bd8c4ca898778
MrSherlok <antomike@mail.ru>
2016-08-03 13:23:53 +0300

    damage upgrade for cookie

--- Assets/Scripts/Player/ShotScript.cs
+++ Assets/Scripts/Player/ShotScript.cs
@@ -6,6 +6,7 @@ public class ShotScript : MonoBehaviour {
 	private float removeTime = 0;
 	public int damage = 1;
 	int[] damDamage = new int[3];
+	int[] cookieDamage = new int[3];
 
 
 //	public bool isEnemyShot = false;
@@ -15,9 +16,13 @@ public class ShotScript : MonoBehaviour {
 		damDamage[1] = 3;
 		damDamage[2] = 5;
 
+		cookieDamage[0] = 2;
+		cookieDamage[1] = 6;
+		cookieDamage[2] = 10;
+
 		if(ChooseSPScript.ActiveDamSpel == 0) damage = damDamage[PlayerPrefs.GetInt("BananaDamageLvl")];
 		if(ChooseSPScript.ActiveDamSpel == 1) damage = damDamage[PlayerPrefs.GetInt("PunchDamageLvl")];
-		if(ChooseSPScript.ActiveDamSpel == 2) damage = damDamage[PlayerPrefs.GetInt("CookieDamageLvl")];
+		if(ChooseSPScript.ActiveDamSpel == 2) damage = cookieDamage[PlayerPrefs.GetInt("CookieDamageLvl")];
 		if(ChooseSPScript.ActiveDamSpel == 3) damage = damDamage[PlayerPrefs.GetInt("BamBladeDamageLvl")];
 	}
 

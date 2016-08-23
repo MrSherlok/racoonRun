f9e884e2ff0562bfa7c913d6ce8892ed59800ed9
Germetic <nazarvkvk@Mail.ru>
2016-08-23 22:08:58 +0300

    New chose sp scene slowmo good fix new enemy

---
+++ Assets/E_hedgehogLogic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class E_hedgehogLogic : MonoBehaviour {
+
+	Animator ani;
+	void Start () {
+		ani = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+			ani.SetTrigger("Attack");
+		}
+	}
+}
--- Assets/Scripts/TutorialLevel/TutorHint.cs
+++ Assets/Scripts/TutorialLevel/TutorHint.cs
@@ -10,13 +10,17 @@ public class TutorHint : MonoBehaviour {
 	void OnTriggerStay2D (Collider2D col) {
 		if (col.gameObject.tag == "Player") {
 //			Time.timeScale = timeScaleSmoother;
+			Time.timeScale = 0.3f;
+			Time.fixedDeltaTime = Time.timeScale * 0.04f;
 
 			GameObject.Find("HintText").GetComponent<HintTextChanger>().ChangeMessage(LanguageManager.Instance.GetTextValue(hintText));
 		}
 	}
 	void OnTriggerExit2D(Collider2D col){
 		if (col.gameObject.tag == "Player") {
-//			Time.timeScale = 1f;
+			//			Time.timeScale = 0.5;
+			Time.timeScale = 1;
+			Time.fixedDeltaTime = Time.timeScale * 0.02f;
 		}
 	}
 

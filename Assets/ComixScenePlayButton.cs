7d2d64f3487a8271358f00bc1fb766bf6a7d1531
Germetic <nazarvkvk@Mail.ru>
2016-09-01 20:36:10 +0300

    Comix start scene

---
+++ Assets/ComixScenePlayButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ComixScenePlayButton : MonoBehaviour {
+
+	// Use this for initialization
+	public void Skip () {
+		SceneManager.LoadScene("LoadScene");
+	}
+	
+
+}
--- Assets/Scripts/Map/Choselvl.cs
+++ Assets/Scripts/Map/Choselvl.cs
@@ -6,13 +6,19 @@ using UnityEngine.SceneManagement;
 public class Choselvl : MonoBehaviour {
 	AudioSource clickSound;
 	public bool itsTutorialLevel = false;
+	//Если истина - включаем сценку с комиксом
+	public bool isComixScene = false;
 	void Start(){
 		clickSound = GameObject.Find ("SoundBox").GetComponent<AudioSource> ();
 	}
 
 	public void GoToChoseSP () {
 		PlayerPrefs.SetString("ChosingLevel",gameObject.name);
-		Invoke("GoToChoseSpels",0.2f);
+		if (!itsTutorialLevel) {
+			Invoke ("GoToChoseSpels", 0.1f);
+		} else {
+			Invoke ("GoToComixScene", 0.1f);
+		}
 		clickSound.Play ();
 
 	}
@@ -22,5 +28,8 @@ public class Choselvl : MonoBehaviour {
 		} else {
 			SceneManager.LoadScene ("chooseSP");
 		}
+	}
+	void GoToComixScene(){
+		SceneManager.LoadScene (PlayerPrefs.GetString("ChosingLevel")+"Comix");
 	}
 }
